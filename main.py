5050208df78f9e7910d39e736c751c334d4e4cd6
arnavrealcoder <arnath.gautam.1515@gmail.com>
2025-12-07 20:59:18 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+num9 = 9
+num10 = 18
+num11 = 27
+num12 = 36
+num13 = 45
+num14 = 54
+num15 = 63
+num16 = 72
+num17 = 81
+num18 = 90
+
+
+print(f"9 x 1 is {num9}")
+print(f"9 x 2 is {num10}")
+print(f"9 x 3 is {num11}")
+print(f"9 x 4 is {num12}")
+print(f"9 x 5 is {num13}")
+print(f"9 x 6 is {num14}")
+print(f"9 x 7 is {num15}")
+print(f"9 x 8 is {num16}")
+print(f"9 x 9 is {num17}")
+print(f"9 x 9 is {num18}")
+
